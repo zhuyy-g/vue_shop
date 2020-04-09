26a4a46729f0e3900cef6d5f3be871d6c385fe18
zhuyy <1961640360@qq.com>
2020-04-09 19:42:12 +0800

    完成登录模块以及用户列表功能的开发

---
+++ src/components/Home.vue
@@ -0,0 +1,130 @@
+<template>
+  <el-container class="home-container">
+    <!--头部区域-->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--页面主体区域-->
+    <el-container>
+      <!--侧边栏-->
+      <el-aside width="200px">
+        <el-menu background-color="#333744" text-color="#fff"
+        active-text-color="#409BFF" unique-opened :collapse="isCollapse"
+        :collapse-transition="false" router :default-active="activePath">
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id"
+            @click="saveNavState('/' + subItem.path)">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--右侧内容主体-->
+      <el-container>
+        <el-main>
+          <!-- 路由占位符 -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      // 一级菜单的图标列表
+      iconObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: ''
+    }
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.menulist = res.data
+    },
+    // 点击按钮，切换链接的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  /*下面样式可以使同级元素定在父级元素左右两边显示*/
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.iconfont {
+  margin-right: 15px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入向后台请求的插件
+import axios from 'axios'
+
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://106.12.11.162:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
