54952aaedddfd20c94a34bd4d960375abf8e82ea
zhuyy <1961640360@qq.com>
2020-04-11 17:41:17 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 // 导入向后台请求的插件
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://106.12.11.162:8888/api/private/v1/'
@@ -18,6 +19,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
