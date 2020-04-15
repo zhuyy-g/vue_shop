c3cf8a8ca07846626b15dff8c376d36e3c43759e
zhuyy <1961640360@qq.com>
2020-04-15 20:22:22 +0800

    '完成商品功能的开发'

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import './assets/fonts/iconfont.css'
 // 导入向后台请求的插件
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://106.12.11.162:8888/api/private/v1/'
@@ -21,6 +27,18 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+Vue.use(VueQuillEditor)
+
+Vue.filter('dataFormat', function(originVal) {
+  const dt = new Date(originVal)
+  console.log(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d}`
+})
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -8,9 +8,10 @@ import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
 import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
+import GoodsList from './components/goods/List.vue'
+import Add from './components/goods/Add.vue'
 
 Vue.use(Router)
-
 var router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
@@ -25,7 +26,9 @@ var router = new Router({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add }
       ]
     }
   ]
